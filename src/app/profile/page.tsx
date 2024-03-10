8a405367a5d4c630827e5128d4c38362089c7462
Md Niyaz Alam <alamniyaz173@gmail.com>
2024-03-10 17:31:04 +0530

    logout functionality is working

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+"use client";
+import React from 'react';
+import axios from 'axios';
+import toast, { Toaster } from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
 
 const page = () => {
+  const router = useRouter();
+  async function handleLogout() {
+    try {
+      const response = await axios.get("/api/users/logout");
+      toast.success('logout Successfully!!');
+      router.push("/login")
+    } catch (error: any) {
+      toast.error("something went wrong", error.message);
+    }
+  };
+
   return (
     <>
-    <h1>Welcoome! to profile page</h1>
+      <div className='_btnLogout'>
+        <button onClick={handleLogout}>Logout</button>
+      </div>
+      <h1>Welcoome! to profile page</h1>
+      <Toaster />
     </>
   )
 }
